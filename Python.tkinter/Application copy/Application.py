566db1c3fbd9b6f656653215ea1b4b9b993f8b2b
Gammasoft71 <gammasoft71@gmail.com>
2019-01-16 20:46:41 +0100

    add ApplicationIcon example

---
+++ Python.tkinter/Application copy/Application.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*
+
+import tkinter
+
+class Program:
+  def main(self=None):
+    application = tkinter.Tk()
+    application.mainloop()
+
+if __name__ == '__main__':
+  Program.main()
