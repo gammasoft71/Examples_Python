2c8298e351c00f67b1bba4ad33a1920a310180a5
gammasoft71 <gammasoft71@gmail.com>
2019-01-16 21:24:48 +0100

    fix ApplicationIcon example

--- Python.tkinter/ApplicationIcon/ApplicationIcon.py
+++ Python.tkinter/ApplicationIcon/ApplicationIcon.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # -*-coding:utf-8 -*
 
+import os
 import tkinter
 
 class Program:
   def main(self=None):
     application = tkinter.Tk()
-    application.iconbitmap('Python.ico')
+    application.iconbitmap('Resources/Python.ico' if os.path.isfile('Resources/Python.ico') else None)
     application.mainloop()
 
 if __name__ == '__main__':
   Program.main()
+  
