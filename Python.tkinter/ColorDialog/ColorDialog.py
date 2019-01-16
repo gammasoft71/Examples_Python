ee996f24eded0e68c6527be300edaed822a87e34
Gammasoft71 <gammasoft71@gmail.com>
2019-01-16 22:12:06 +0100

    add ColorDialog example

---
+++ Python.tkinter/ColorDialog/ColorDialog.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*
+
+import tkinter
+import tkinter.colorchooser
+import tkinter.ttk
+
+class Form1(tkinter.Tk):
+  def __init__(self):
+    super().__init__()
+
+    self.button = tkinter.ttk.Button(self, command=self.OnColorClick, text="Color...")
+    self.button.place(x=10, y=10)
+
+    self.geometry("300x300+200+100")
+    self.title("ColorDialog example")
+
+  def main(self=None):
+    form = Form1()
+    form.mainloop()
+
+  def OnColorClick(self):
+    color = tkinter.colorchooser.askcolor(color=self['background'])
+    if color[1]:
+      self['background'] = color[1]
+
+if __name__ == '__main__':
+  Form1.main()
