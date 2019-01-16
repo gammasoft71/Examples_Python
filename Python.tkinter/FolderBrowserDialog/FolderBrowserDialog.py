c1d37be0ee16cc9f8769dd6eb3c75db7571e8e64
Gammasoft71 <gammasoft71@gmail.com>
2019-01-16 22:44:09 +0100

    add FolderBrowserDialog example

---
+++ Python.tkinter/FolderBrowserDialog/FolderBrowserDialog.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*
+
+import os
+import platform
+import tkinter
+import tkinter.filedialog
+import tkinter.ttk
+
+class Form1(tkinter.Tk):
+  def __init__(self):
+    super().__init__()
+
+    self.frame = tkinter.ttk.Frame(self)
+    self.frame.pack(fill = tkinter.BOTH, expand=1)
+
+    self.button = tkinter.ttk.Button(self.frame, command=self.OnColorClick, text="Folder...")
+    self.button.place(x=10, y=10)
+
+    self.label = tkinter.ttk.Label(self.frame, text="Path = ")
+    self.label.place(x=10, y=40)
+
+    self.geometry("300x300+200+100")
+    self.title("FolderBrowser example")
+
+  def main(self=None):
+    form = Form1()
+    form.mainloop()
+
+  def OnColorClick(self):
+    directory = tkinter.filedialog.askdirectory(initialdir=os.path.join(os.environ["HOMEPATH" if platform.system() == 'Windows' else 'HOME'], "Desktop"))
+    if directory:
+      self.label['text'] = directory
+
+if __name__ == '__main__':
+  Form1.main()
