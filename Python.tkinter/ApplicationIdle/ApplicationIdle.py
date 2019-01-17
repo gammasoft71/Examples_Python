4ffe454708a2ca135d9b4a7dcfe5b2668d0b0977
Gammasoft71 <gammasoft71@gmail.com>
2019-01-17 12:57:27 +0100

    Fix ApplicationIdle

--- Python.tkinter/ApplicationIdle/ApplicationIdle.py
+++ Python.tkinter/ApplicationIdle/ApplicationIdle.py
@@ -9,13 +9,15 @@ class Form1(tkinter.Tk):
     super().__init__()
     
     self.counter = 0
+    self.destroyed = False
 
-    self.geometry("300x300+200+100")
-    self.title("")
+    self.geometry('300x300+200+100')
+    self.protocol('WM_DELETE_WINDOW', self.OnQuit)
+    self.title('')
     
   def main(self=None):    
     form = Form1()
-    while True:
+    while not form.destroyed:
       form.after_idle(form.OnIdle)
       form.update_idletasks()
       form.update()
@@ -23,6 +25,10 @@ class Form1(tkinter.Tk):
   def OnIdle(self):
     self.counter += 1
     self.title(str(self.counter))
+
+  def OnQuit(self):
+    self.destroyed = True
+    self.destroy()
 
 if __name__ == '__main__':
   Form1.main()
