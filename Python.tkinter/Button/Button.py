b806077c7c499570caac90fae7b8e4516061ca39
Gammasoft71 <gammasoft71@gmail.com>
2019-01-24 16:55:31 +0100

    rename frame by panem

--- Python.tkinter/Button/Button.py
+++ Python.tkinter/Button/Button.py
@@ -11,22 +11,22 @@ class Form1(tkinter.Tk):
     self.button1Clicked = 0
     self.button2Clicked = 0
 
-    self.frame = tkinter.ttk.Frame(self)
-    self.frame.pack(fill = tkinter.BOTH, expand=1)
+    self.panel = tkinter.ttk.Frame(self)
+    self.panel.pack(fill = tkinter.BOTH, expand=1)
 
-    self.button1 = tkinter.ttk.Button(self.frame, command=self.OnButton1Click, text="button1")
+    self.button1 = tkinter.ttk.Button(self.panel, command=self.OnButton1Click, text="button1")
     self.button1.place(x=50, y=50)
     
-    self.frameButton = tkinter.ttk.Frame(self.frame, width=200, height=75)
+    self.frameButton = tkinter.ttk.Frame(self.panel, width=200, height=75)
     self.frameButton.pack_propagate(0)
     self.frameButton.place(x = 50, y = 100)
     self.button2 = tkinter.ttk.Button(self.frameButton, command=self.OnButton2Click, text="button2")
     self.button2.pack(fill = tkinter.BOTH, expand=1)
     
-    self.label1 = tkinter.ttk.Label(self.frame, text="button1 clicked {0} times".format(self.button1Clicked))
+    self.label1 = tkinter.ttk.Label(self.panel, text="button1 clicked {0} times".format(self.button1Clicked))
     self.label1.place(x=50, y=200) 
     
-    self.label2 = tkinter.ttk.Label(self.frame, text="button2 clicked {0} times".format(self.button2Clicked))
+    self.label2 = tkinter.ttk.Label(self.panel, text="button2 clicked {0} times".format(self.button2Clicked))
     self.label2.place(x=50, y=230) 
 
     self.geometry("300x300+200+100")
--- Python.tkinter/CheckBox/CheckBox.py
+++ Python.tkinter/CheckBox/CheckBox.py
@@ -8,22 +8,22 @@ class Form1(tkinter.Tk):
   def __init__(self):
     super().__init__()
   
-    self.frame = tkinter.ttk.Frame(self)
-    self.frame.pack(fill = tkinter.BOTH, expand=1)
+    self.panel = tkinter.ttk.Frame(self)
+    self.panel.pack(fill = tkinter.BOTH, expand=1)
 
     self.checkBoxVar1 = tkinter.BooleanVar()
     self.checkBoxVar1.set(True)
     
-    self.checkBox1 = tkinter.ttk.Checkbutton(self.frame, text="Checked", variable=self.checkBoxVar1)
+    self.checkBox1 = tkinter.ttk.Checkbutton(self.panel, text="Checked", variable=self.checkBoxVar1)
     self.checkBox1.place(x = 30, y = 30) 
     
     self.checkBoxVar2 = tkinter.BooleanVar()
     self.checkBoxVar2.set(False)
     
-    self.checkBox2 = tkinter.ttk.Checkbutton(self.frame, text="Unchecked", variable=self.checkBoxVar2)
+    self.checkBox2 = tkinter.ttk.Checkbutton(self.panel, text="Unchecked", variable=self.checkBoxVar2)
     self.checkBox2.place(x = 30, y = 60) 
     
-    self.checkBox3 = tkinter.ttk.Checkbutton(self.frame, text="Indeterminate", variable=None)
+    self.checkBox3 = tkinter.ttk.Checkbutton(self.panel, text="Indeterminate", variable=None)
     self.checkBox3.place(x = 30, y = 90) 
 
     self.geometry("300x300+200+100")
