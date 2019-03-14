86eda18f88762bda40a0c035859c4d1346aa2736
Gammasoft71 <gammasoft71@gmail.com>
2019-03-14 11:33:28 +0100

    add PictureBox example

---
+++ Python.tkinter/PictureBox/PictureBox.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*
+
+# You must install Pillow package before use it :
+# pip install Pillow
+# More information :  https://pillow.readthedocs.io/en/3.0.x/index.html
+
+import tkinter
+import tkinter.font
+import tkinter.ttk
+import PIL.ImageTk
+import PIL.Image
+
+class Form1(tkinter.Tk):
+  def __init__(self):
+    super().__init__()
+    
+    self.panel = tkinter.ttk.Frame(self)
+    self.panel.pack(fill = tkinter.BOTH, expand=1)
+
+    self.image1 = PIL.ImageTk.PhotoImage(PIL.Image.open("Logo.png"))
+    self.pictureBox1 = tkinter.ttk.Label(self.panel, image=self.image1, borderwidth=2, relief="groove")
+    self.pictureBox1.place(x=20, y=20)
+
+    self.geometry("300x300+200+100")
+    self.title("PictureBox example")
+
+  def main(self=None):
+    form = Form1()
+    form.mainloop()
+
+if __name__ == '__main__':
+  Form1.main()
