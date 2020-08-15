8935bbb28f2fe77f1fac66c9d9c48ce783404751
Gammasoft71 <gammasoft71@gmail.com>
2020-08-15 11:51:03 +0200

    add examples

---
+++ Python.wx/Application/ApplicationAndMain/ApplicationAndMain.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*
+
+import wx
+
+if __name__ == '__main__':
+  application = wx.App()
+  wx.Frame(None).Show()
+  application.MainLoop()
--- Python.wx/Application/ApplicationIcon/ApplicationIcon.py
+++ Python.wx/Application/ApplicationIcon/ApplicationIcon.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 # -*-coding:utf-8 -*
 
+import os
 import wx
 
 class Program:
   def main(self=None):
     application = wx.App()
-    wx.Frame(None).Show()
+    frame = wx.Frame(None)
+    frame.SetIcon(wx.Icon(os.path.join(os.path.dirname(os.path.abspath(__file__)), "Resources", "Gammasoft.ico")))
+    frame.Show()
     application.MainLoop()
 
 if __name__ == '__main__':
