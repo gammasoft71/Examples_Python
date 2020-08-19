b6618569e74ab951df061a31d4ad93ed586509ce
Gammasoft71 <gammasoft71@gmail.com>
2020-08-19 15:53:40 +0200

    Update CommandLinkButton.py

--- wxPython/CommonControls/CommandLinkButton/CommandLinkButton.py
+++ wxPython/CommonControls/CommandLinkButton/CommandLinkButton.py
@@ -9,9 +9,9 @@ class Frame1(wx.Frame):
     super().__init__(None, wx.ID_ANY, 'Command link button example', wx.DefaultPosition, wx.Size(300, 300))
     self.commandLinkButton1Clicked = 0
     self.panel = wx.Panel(self)
-    self.commandLinkButton1 = wx.adv.CommandLinkButton(self.panel, wx.ID_ANY, 'Link', 'Information text', wx.Point(30, 30), wx.Size(150, 40))
+    self.commandLinkButton1 = wx.adv.CommandLinkButton(self.panel, wx.ID_ANY, 'Link', 'Information text', wx.Point(30, 30), wx.Size(200, 60))
     self.commandLinkButton1.Bind(wx.EVT_BUTTON, self.OnCommandLinkButton1Click)
-    self.staticText1 = wx.StaticText(self.panel, wx.ID_ANY, 'commandLinkButton1 clicked 0 times', wx.Point(30, 100), wx.Size(200, 20))
+    self.staticText1 = wx.StaticText(self.panel, wx.ID_ANY, 'commandLinkButton1 clicked 0 times', wx.Point(30, 100), wx.Size(200, 50))
     
   def OnCommandLinkButton1Click(self, event):
     self.commandLinkButton1Clicked += 1
